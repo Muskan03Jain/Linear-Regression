76a5d946b08ff7b45dc082f3113760210bd6fb3e
Muskan Jain <166038528+Muskan03Jain@users.noreply.github.com>
2024-08-01 23:59:15 +0530

    Add files via upload

---
+++ Medical Insurance/app.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import joblib
+import streamlit as st
+
+insurance_model_pkl = r"C:\Users\Lenovo\Muskan\Ml\github assignment\Machine-Learning-Projects\Linear_Regression\Medical_Insurance\model.pkl"
+loaded_model = joblib.load(insurance_model_pkl)
+
+st.header("Medical Insurance")
+
+age=st.number_input("Enter the Age ")
+
+sex = st.selectbox("Enter the sex", ("Male", "Female"))
+sex_dict={"Male":0, "Female": 1,}
+sex=sex_dict[sex]
+
+children=st.number_input("Enter the number of children")
+
+smoke = st.selectbox("do you smoke", ("Yes", "No"))
+smoke_dict={"Yes":0, "No": 1,}
+smoke=smoke_dict[smoke]
+
+X_new = np.array([[age,sex,children,smoke]])
+
+button = st.button("Submit")
+
+if button:
+
+    result = loaded_model.predict(X_new)
+    result=np.round(result[0])
+
+    st.info("Amount of Insurance Predicted : " + str(result))
